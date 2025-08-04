281295b9f4b8e99bd079050b5d598018557e7c16
memeliciouss <rakshit3raj@gmail.com>
2025-08-05 00:53:23 +0530

    ignore pycache files

--- buyer/forms.py
+++ buyer/forms.py
@@ -6,3 +6,11 @@ class newBuyerForm(forms.ModelForm):
         model  = buyer
         fields = ['name', 'adr1', 'adr2', 'state', 'pin', 'gstin']
         # fields = '__all__'
+        labels = {
+            'name': 'Name',
+            'adr1': 'Address Line 1',
+            'adr2': 'Address Line 2',
+            'state': 'State',
+            'pin': 'PIN Code',
+            'gstin': 'GSTIN'
+        }
